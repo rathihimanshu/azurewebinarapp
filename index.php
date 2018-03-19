a4543408bac07e9333196176966220ac016abdc6
himanshu <rathihimanshu@live.com>
2018-03-19 20:30:49 +0530

    third

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body bgcolor="#FFFF00">
 <center><h1>PRODUCTION</h1></center>
 <br><br>
-<center><h2>APP NAME IS --> <?php echo getenv("application"); ?></h2></center>
+<center><h2>APP NAME IS --> <?php echo getenv("appname"); ?></h2></center>
 <center><h2>ENV NAME IS --> <?php echo getenv("environment"); ?></h2></center>
 </body>
 </html>
