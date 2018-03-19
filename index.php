4c9e924ced64adefef00e961ddbd9d2a17262595
himanshu <rathihimanshu@live.com>
2018-03-19 20:29:09 +0530

    third

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <body bgcolor="#FFFF00">
 <center><h1>PRODUCTION</h1></center>
 <br><br>
-second commit
+<center><h2>APP NAME IS --> <?php echo getenv("application"); ?></h2></center>
+<center><h2>ENV NAME IS --> <?php echo getenv("environment"); ?></h2></center>
 </body>
 </html>
